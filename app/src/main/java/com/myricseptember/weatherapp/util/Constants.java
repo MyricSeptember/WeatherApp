4d883da26b80ae6f3543fd48ed77bd683a52c779
Myric September <myric.september@gmail.com>
2018-05-15 17:49:01 +0200

    Update Constants.java

--- app/src/main/java/com/myricseptember/weatherapp/util/Constants.java
+++ app/src/main/java/com/myricseptember/weatherapp/util/Constants.java
@@ -6,6 +6,6 @@ package com.myricseptember.weatherapp.util;
 
 public class Constants {
     public static String BASE_URL = "http://api.openweathermap.org/data/2.5/";
-    public static String OPEN_WEATHER_KEY = "c01f5218f214c82f32e5c30d33719b40";
+    public static String OPEN_WEATHER_KEY = "YOUR API KEY HERE";
     public static WeatherUnits WEATHER_UNITS = WeatherUnits.METRIC;
 }
